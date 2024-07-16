b1f41a981681a8f422f71278c09bb5625f8b0676
fadhillahramadhan <fadhillahramadhan01@gmail.com>
2024-07-16 13:07:33 +0700

    chore: Update service worker cache URLs

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,45 +1,45 @@
 const CACHE_NAME = 'v1_cache';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/css/app.css',
-  '/images/logo.png',
-  // Add more files you want to cache
+	'/portofolio-static',
+	'/portofolio-static/css/app.css',
+	'/portofolio-static/images/logo.png',
+	'https://threejs.org/examples/js/libs/stats.min.js',
+	'https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js',
+	'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js',
+	// Add more files you want to cache
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			console.log('Opened cache');
+			return cache.addAll(urlsToCache);
+		})
+	);
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			if (response) {
+				return response;
+			}
+			return fetch(event.request);
+		})
+	);
 });
 
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('activate', (event) => {
+	const cacheWhitelist = [CACHE_NAME];
+	event.waitUntil(
+		caches.keys().then((cacheNames) => {
+			return Promise.all(
+				cacheNames.map((cacheName) => {
+					if (cacheWhitelist.indexOf(cacheName) === -1) {
+						return caches.delete(cacheName);
+					}
+				})
+			);
+		})
+	);
 });
